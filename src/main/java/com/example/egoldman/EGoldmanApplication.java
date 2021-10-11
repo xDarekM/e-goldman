62f3446f6ec0c95f8ca19f6b28b2a4f09b49b5c3
Dariusz Marciniak <darekmarciniak69@gmail.com>
2021-10-11 21:20:23 +0200

    initial commit

---
+++ src/main/java/com/example/egoldman/EGoldmanApplication.java
@@ -0,0 +1,13 @@
+package com.example.egoldman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EGoldmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EGoldmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/egoldman/EGoldmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.egoldman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EGoldmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
